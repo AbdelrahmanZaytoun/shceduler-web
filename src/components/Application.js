af5c15bfccc1a83cdde5411c572e44ab4ea13168
AbdelrahmanZaytoun <abdelrahmanzaytounn@gmail.com>
2022-01-10 01:09:20 +0300

    Add InterviewerListItem

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,16 +1,33 @@
 import React from "react";
-
 import "components/Application.scss";
 
+import DayList from "./DayList";
+
+import useApplicationData from "hooks/useApplicationData";
+
 export default function Application(props) {
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
+
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList days={state.days} day={state.day} setDay={setDay} />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule"></section>
     </main>
   );
 }
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,61 @@
+import React, { useReducer, useEffect } from "react";
+import axios from "axios";
+import reducer, {
+  SET_DAY,
+  SET_APPLICATION_DATA,
+  SET_INTERVIEW,
+} from "reducers/application";
+
+export default function useApplicationData() {
+  const [state, dispatch] = useReducer(reducer, {
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {},
+  });
+
+  const setDay = (day) => dispatch({ type: SET_DAY, day });
+
+  useEffect(() => {
+    const daysData = axios.get("/api/days");
+    const appointmentsData = axios.get("/api/appointments");
+    const interviewersData = axios.get("/api/interviewers");
+    Promise.all([daysData, appointmentsData, interviewersData]).then((all) => {
+      dispatch({
+        type: SET_APPLICATION_DATA,
+        days: all[0].data,
+        appointments: all[1].data,
+        interviewers: all[2].data,
+      });
+    });
+  }, []);
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview,
+    };
+
+    return axios.put(`/api/appointments/${id}`, appointment).then(() => {
+      dispatch({ type: SET_INTERVIEW, id, interview });
+    });
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+
+    return axios.delete(`/api/appointments/${id}`, appointment).then(() => {
+      dispatch({ type: SET_INTERVIEW, id, interview: null });
+    });
+  }
+
+  return {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview,
+  };
+}
