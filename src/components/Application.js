052bd52239e880bf9c86c8d69a971d1b0a70d1e3
AbdelrahmanZaytoun <abdelrahmanzaytounn@gmail.com>
2022-01-10 01:10:26 +0300

    Add appoinments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,12 +2,32 @@ import React from "react";
 import "components/Application.scss";
 
 import DayList from "./DayList";
+import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
+import { getInterview } from "helpers/selectors";
+import { getInterviewersForDay } from "helpers/selectors";
 
 import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
   const { state, setDay, bookInterview, cancelInterview } =
     useApplicationData();
+
+  const appointments = getAppointmentsForDay(state, state.day);
+
+  const appointmentList = appointments.map((appointment) => {
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={getInterview(state, appointment.interview)}
+        interviewers={getInterviewersForDay(state, state.day)}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
+    );
+  });
 
   return (
     <main className="layout">
@@ -27,7 +47,10 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule"></section>
+      <section className="schedule">
+        {appointmentList}
+        <Appointment key="last" time="5pm" />
+      </section>
     </main>
   );
 }
